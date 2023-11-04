63ff10fc698b62c290b9f9cc53473b7e04ea7dae
Mohammadsadegh Mazaheri <42312695+sadegh1998@users.noreply.github.com>
2023-11-04 13:57:45 +0000

    add model price

---
+++ MvcResume/Models/Pricing.cs
@@ -0,0 +1,9 @@
+namespace MvcResume.Models
+{
+    public long ID { get; set; }
+     public string Plan { get; set; }
+      public decimal Price { get; set; }
+       public string Period { get; set; }
+        public string Link { get; set; }
+        public string Description { get; set; }
+}
