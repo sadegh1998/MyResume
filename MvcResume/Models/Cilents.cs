9ad306e2ff11ffe01d75df2e9e6cb7cebdcb83cf
Mohammadsadegh Mazaheri <42312695+sadegh1998@users.noreply.github.com>
2023-11-04 13:57:25 +0000

    add model client

---
+++ MvcResume/Models/Cilents.cs
@@ -0,0 +1,7 @@
+namespace MvcResume.Models
+{
+   public long ID { get; set; }
+   public string Image { get; set; }
+   public string ClientName { get; set; } 
+   public string Url { get; set; }
+}
