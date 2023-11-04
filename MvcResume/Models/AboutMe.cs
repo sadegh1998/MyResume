5d09a513b61b78c5c9fae15b99f96d21016c79ea
Mohammadsadegh Mazaheri <42312695+sadegh1998@users.noreply.github.com>
2023-11-04 13:57:05 +0000

    add model about me

---
+++ MvcResume/Models/AboutMe.cs
@@ -0,0 +1,13 @@
+namespace MvcResume.Models{
+        public class AboutMe
+    {
+        public long ID { get; set; }
+        public string Description { get; set; }
+        public int Age { get; set; }
+        public string Residence { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
