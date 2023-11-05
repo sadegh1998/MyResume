8cad2cb7df0d89117e286560232133e887a0b054
Mohammadsadegh Mazaheri <42312695+sadegh1998@users.noreply.github.com>
2023-11-05 05:56:17 +0000

    fix bug models .

--- MvcResume/Models/Cilents.cs
+++ MvcResume/Models/Cilents.cs
@@ -1,7 +1,10 @@
 namespace MvcResume.Models
 {
-   public long ID { get; set; }
+    public class Client{
+ public long ID { get; set; }
    public string Image { get; set; }
    public string ClientName { get; set; } 
    public string Url { get; set; }
+    }
+  
 }
--- MvcResume/Models/Pricing.cs
+++ MvcResume/Models/Pricing.cs
@@ -1,9 +1,12 @@
 namespace MvcResume.Models
 {
-    public long ID { get; set; }
+    public class Pricing{
+public long ID { get; set; }
      public string Plan { get; set; }
       public decimal Price { get; set; }
        public string Period { get; set; }
         public string Link { get; set; }
         public string Description { get; set; }
+    }
+    
 }
