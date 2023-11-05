2531cc31d86fa6318bfd54102be875fe5b127aec
Mohammadsadegh Mazaheri <42312695+sadegh1998@users.noreply.github.com>
2023-11-05 10:24:06 +0000

    Add model Experience

---
+++ MvcResume/Models/Experience.cs
@@ -0,0 +1,13 @@
+namespace MvcResume.Models { 
+public class Experience{
+     public long ID { get; set; }
+
+     public DateTime FromDate { get; set; }
+     public DateTime ToDate { get; set; }
+     public string Company { get; set; }
+     public string JobTitle { get; set; }
+     public string Description { get; set; }
+
+}
+
+}
