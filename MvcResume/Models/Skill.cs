a6911fe61790545980617fc58f7ccea7eed5acc9
Mohammadsadegh Mazaheri <42312695+sadegh1998@users.noreply.github.com>
2023-11-05 10:24:35 +0000

    Add model Skill

---
+++ MvcResume/Models/Skill.cs
@@ -0,0 +1,10 @@
+namespace MvcResume.Models { 
+public class Skill{
+     public long ID { get; set; }
+     public string Title { get; set; }
+     public int Percentage { get; set; }
+    public int Type { get; set; }
+
+}
+
+}
