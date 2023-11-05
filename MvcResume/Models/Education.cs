fe07af437b4629bc6b631d6e7d7f3d91ea78f434
Mohammadsadegh Mazaheri <42312695+sadegh1998@users.noreply.github.com>
2023-11-05 10:23:53 +0000

    Add model Education

---
+++ MvcResume/Models/Education.cs
@@ -0,0 +1,9 @@
+namespace MvcResume.Models {
+    public class Education{
+        public long ID { get; set; }
+   public DateTime GraduationDate { get; set; }
+        public string UniversityName { get; set; }
+ public string FieldOfStudy {get;set;}
+    public string Description {get;set;}
+    }
+}
