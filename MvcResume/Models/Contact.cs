6e4a52cc9929e159fe1a16472ae38b8161652fdb
Mohammadsadegh Mazaheri <42312695+sadegh1998@users.noreply.github.com>
2023-11-05 10:34:04 +0000

    Add model Contact

---
+++ MvcResume/Models/Contact.cs
@@ -0,0 +1,10 @@
+namespace MvcResume.Models{
+    public class Contact{
+        public long ID { get; set; }
+        public string Location { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public int Status { get; set; }
+         public string Mapsrc { get; set; }
+    }
+}
