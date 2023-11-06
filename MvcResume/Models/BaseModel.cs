296904fdf9d94430f492ba405aa3bfe7afe1bff9
Mohammadsadegh Mazaheri <42312695+sadegh1998@users.noreply.github.com>
2023-11-06 10:05:53 +0000

    Add Base Model

---
+++ MvcResume/Models/BaseModel.cs
@@ -0,0 +1,9 @@
+namespace MvcResume.Models
+{
+    public class BaseModel
+    {
+        public long ID { get; set; } 
+        public DateTime CreateDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+    }
+}
