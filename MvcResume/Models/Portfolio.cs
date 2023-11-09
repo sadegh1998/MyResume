4e23ee4df1192e714899919ebefae727d8015e22
mohammadsadegh mazaheri <sadegh.mazaheri1377@gmail.com>
2023-11-09 13:34:18 +0330

    Add model Portfoloio

---
+++ MvcResume/Models/Portfolio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MvcResume.Models;
+
+public partial class Portfolio
+{
+    public long Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Url { get; set; } = null!;
+
+    public string ProjectName { get; set; } = null!;
+
+    public string Image { get; set; } = null!;
+
+    public string? Video { get; set; }
+}
