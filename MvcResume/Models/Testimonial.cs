868b79bd4f554b0a7cc415a4407c8c90bf4e6b9e
mohammadsadegh mazaheri <sadegh.mazaheri1377@gmail.com>
2023-11-09 13:34:54 +0330

    Add model testimonial

---
+++ MvcResume/Models/Testimonial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MvcResume.Models;
+
+public partial class Testimonial
+{
+    public long Id { get; set; }
+
+    public string Image { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public string ProjectName { get; set; } = null!;
+}
