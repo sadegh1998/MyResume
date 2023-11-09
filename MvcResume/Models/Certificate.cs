ebc95fc855e69cfbd742e63c5160aa5c3ae93528
mohammadsadegh mazaheri <sadegh.mazaheri1377@gmail.com>
2023-11-09 13:33:42 +0330

    Add model certificate

---
+++ MvcResume/Models/Certificate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MvcResume.Models;
+
+public partial class Certificate
+{
+    public long Id { get; set; }
+
+    public string Image { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+
+    public string SerialNumber { get; set; } = null!;
+
+    public DateTime AchiveDate { get; set; }
+
+    public string Company { get; set; } = null!;
+}
