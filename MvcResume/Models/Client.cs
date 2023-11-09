2dcc223347c4507cf79d6601010a2ebebe52a796
mohammadsadegh mazaheri <sadegh.mazaheri1377@gmail.com>
2023-11-09 13:33:48 +0330

    Add model clients

---
+++ MvcResume/Models/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MvcResume.Models;
+
+public partial class Client
+{
+    public long Id { get; set; }
+
+    public string Image { get; set; } = null!;
+
+    public string ClientName { get; set; } = null!;
+
+    public string Url { get; set; } = null!;
+}
