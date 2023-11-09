bc81a36e4065362fa197c71e61d15d03171c67ae
mohammadsadegh mazaheri <sadegh.mazaheri1377@gmail.com>
2023-11-09 13:33:34 +0330

    Add model Blog

---
+++ MvcResume/Models/Blog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MvcResume.Models;
+
+public partial class Blog
+{
+    public long Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public string Image { get; set; } = null!;
+
+    public string? Author { get; set; }
+
+    public string? Tag { get; set; }
+}
