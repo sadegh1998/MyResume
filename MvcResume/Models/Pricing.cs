bdbc71e1c05ee0d832192e88e9dc8c28decdf5d9
mohammadsadegh mazaheri <sadegh.mazaheri1377@gmail.com>
2023-11-09 13:34:24 +0330

    Add model pricinig

---
+++ MvcResume/Models/Pricing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MvcResume.Models;
+
+public partial class Pricing
+{
+    public long Id { get; set; }
+
+    public string PlanName { get; set; } = null!;
+
+    public decimal? Price { get; set; }
+
+    public string Period { get; set; } = null!;
+
+    public string Link { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+}
