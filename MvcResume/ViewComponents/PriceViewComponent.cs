ba3f580da098486fd4be76e8cab9da9c78ee41f1
mohammadsadegh mazaheri <sadegh.mazaheri1377@gmail.com>
2023-11-10 22:27:27 +0330

    Add view component price

---
+++ MvcResume/ViewComponents/PriceViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcResume.Models;
+
+namespace MvcResume.ViewComponents
+{
+    public class PriceViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            ResumeContext context = new ResumeContext();
+            var model = context.Pricings.ToList();
+            return View("_Price",model);
+        }
+    }
+}
