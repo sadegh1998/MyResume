3fe2ec06950507975bc1f84b7e77358661040c89
mohammadsadegh mazaheri <sadegh.mazaheri1377@gmail.com>
2023-11-10 22:40:58 +0330

    Add blog view component

---
+++ MvcResume/ViewComponents/BlogsViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcResume.Models;
+
+namespace MvcResume.ViewComponents
+{
+    public class BlogsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            ResumeContext context = new ResumeContext();    
+            var blogs  = context.Blogs.ToList();
+                return View("_Blogs",blogs);
+        }
+    }
+}
