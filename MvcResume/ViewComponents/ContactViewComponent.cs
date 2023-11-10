ab18a65967aaeecd6002f9d280b9cd1dc4bc833a
mohammadsadegh mazaheri <sadegh.mazaheri1377@gmail.com>
2023-11-10 22:55:01 +0330

    Add contact view component

---
+++ MvcResume/ViewComponents/ContactViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcResume.Models;
+
+namespace MvcResume.ViewComponents
+{
+    public class ContactViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            ResumeContext context = new ResumeContext();
+            var contactmodel = context.Contacts.FirstOrDefault();    
+            return View("_Contact",contactmodel);
+        }
+    }
+}
