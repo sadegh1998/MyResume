a98ab4688806696b4143d1d93e44fc59f29f5dc7
mohammadsadegh mazaheri <sadegh.mazaheri1377@gmail.com>
2023-11-10 22:20:21 +0330

    Add clients view component .

---
+++ MvcResume/ViewComponents/ClientsViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcResume.Models;
+
+namespace MvcResume.ViewComponents
+{
+    public class ClientsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            ResumeContext context = new ResumeContext();
+            var Clients = context.Clients.ToList();
+            return View("_Clients",Clients);
+        }
+    }
+}
