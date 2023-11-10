425b657b47683ec6134a945e8e3e3cc3eeccc125
mohammadsadegh mazaheri <sadegh.mazaheri1377@gmail.com>
2023-11-10 22:12:19 +0330

    add Testimonials view component

---
+++ MvcResume/ViewComponents/TestimonialsViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcResume.Models;
+
+namespace MvcResume.ViewComponents
+{
+    public class TestimonialsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            ResumeContext context = new ResumeContext();
+            var Testimonials = context.Testimonials.ToList();
+            return View("_Testimonials", Testimonials);  
+        }
+    }
+}
