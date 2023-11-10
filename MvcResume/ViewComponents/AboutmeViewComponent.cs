67b9c53f82a8aad0c84c748112175f685b53aad0
mohammadsadegh mazaheri <sadegh.mazaheri1377@gmail.com>
2023-11-10 22:04:25 +0330

    Add viewcomponent Aboutme .

---
+++ MvcResume/ViewComponents/AboutmeViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcResume.Models;
+
+namespace MvcResume.ViewComponents
+{
+    public class AboutmeViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            ResumeContext context = new ResumeContext();
+            var model = context.AboutMes.FirstOrDefault();
+            return View("_Aboutme",model);
+        }
+    }
+}
