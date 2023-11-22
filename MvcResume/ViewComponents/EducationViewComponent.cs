d0ba4e613c8013c222aead2ffdfd517857436e15
mohammadsadegh mazaheri <sadegh.mazaheri1377@gmail.com>
2023-11-22 13:35:51 +0330

    Add ViewComponent education

---
+++ MvcResume/ViewComponents/EducationViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcResume.Models;
+
+namespace MvcResume.ViewComponents
+{
+    public class EducationViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            ResumeContext context = new ResumeContext();
+            var educate = context.Educations.ToList();
+            return View("_Education",educate);
+        }
+    }
+}
