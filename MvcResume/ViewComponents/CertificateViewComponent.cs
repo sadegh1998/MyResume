42d966a857ec43ece0b82151ac79a219755f18cf
mohammadsadegh mazaheri <sadegh.mazaheri1377@gmail.com>
2023-11-22 13:35:34 +0330

    Add ViewComponent Certificate

---
+++ MvcResume/ViewComponents/CertificateViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcResume.Models;
+
+namespace MvcResume.ViewComponents
+{
+    public class CertificateViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            ResumeContext context = new ResumeContext();
+            var model = context.Certificates.ToList();
+            return View("_Certificate",model);
+        }
+    }
+}
