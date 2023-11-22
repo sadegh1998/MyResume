173ab4ce18c86b0f7b3793b922f960e8cbe66187
mohammadsadegh mazaheri <sadegh.mazaheri1377@gmail.com>
2023-11-22 13:36:11 +0330

    Add ViewComponent experience

---
+++ MvcResume/ViewComponents/ExperienceViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcResume.Models;
+
+namespace MvcResume.ViewComponents
+{
+    public class ExperienceViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            ResumeContext context = new ResumeContext();
+            var model = context.Experiences.ToList();   
+            return View("_Experience",model);
+        }
+    }
+}
