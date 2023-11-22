44d40ae0b694ec74d8db01d37cf8a45f93588075
mohammadsadegh mazaheri <sadegh.mazaheri1377@gmail.com>
2023-11-22 13:36:48 +0330

    Add ViewComponent portfolio

---
+++ MvcResume/ViewComponents/PortfolioViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcResume.Models;
+
+namespace MvcResume.ViewComponents
+{
+    public class PortfolioViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            ResumeContext context = new ResumeContext();
+            var model = context.Portfolios.ToList();
+            return View("_Portfolio", model);
+        }
+    }
+}
