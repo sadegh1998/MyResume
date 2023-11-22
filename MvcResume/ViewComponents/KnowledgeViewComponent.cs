edc7133de016751783841d001ecb8240b69ee3ba
mohammadsadegh mazaheri <sadegh.mazaheri1377@gmail.com>
2023-11-22 13:36:33 +0330

    Add ViewComponent knowledge

---
+++ MvcResume/ViewComponents/KnowledgeViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcResume.Models;
+
+namespace MvcResume.ViewComponents
+{
+    public class KnowledgeViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            ResumeContext context = new ResumeContext();
+            var model = context.Knowledges.ToList();
+            return View("_Knowledge",model);
+        }
+    }
+}
